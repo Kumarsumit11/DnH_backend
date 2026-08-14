3f2c4b5bf71a361b59589c1c57aec90612d641f6
Kumarsumit11 <info2sumitkumar@gmail.com>
2026-08-14 23:21:58 +0530

    admin block2 cors

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,7 @@ app.use(
         "https://www.dnhfintech.com/",
         "http://localhost:8443",
         "https://www.dnhfintech.com",
+        "https://admin-dnh.vercel.app",
         "https://dnhadmin.vercel.app",
         
       ];
